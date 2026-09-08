478334842533c722a5040df79c661c541327582c
kaninikusan <kanito0930@gmail.com>
2026-09-08 22:33:35 +0900

    表示文字変更

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      <Text>Hello World1</Text>
     </View>
   );
 }
