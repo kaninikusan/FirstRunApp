0ebba918a1936e5f6c6f707e0246496b07c210aa
kaninikusan <kanito0930@gmail.com>
2026-09-19 16:23:26 +0900

    gittest

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -31,7 +31,7 @@ export default function TabLayout() {
         options={{
           title: "About",
           tabBarIcon: ({ color, focused }) => (
-            <Feather name="filter" size={24} color="#008b3a" />
+            <Feather name="filter" size={24} color="#209450" />
           ),
         }}
       />
